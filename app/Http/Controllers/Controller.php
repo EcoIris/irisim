9af5cc37048603b80b226a582cd72361ee8d8556
Zheng Shuaijie <962224495@qq.com>
2020-05-08 17:33:38 +0800

    优化上次提交

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -53,7 +53,7 @@ class Controller extends BaseController
     {
         //建立socket连接到内部推送端口
         $client = stream_socket_client('tcp://127.0.0.1:3121', $errno, $errmsg, 1);
-        // 发送数据，注意5678端口是Text协议的端口，Text协议需要在数据末尾加上换行符
+        // 发送数据，注意3121端口是Text协议的端口，Text协议需要在数据末尾加上换行符
         fwrite($client, json_encode($data, JSON_UNESCAPED_UNICODE)."\n");
     }
 
